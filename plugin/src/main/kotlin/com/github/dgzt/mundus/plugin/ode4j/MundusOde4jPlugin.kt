80195e42a6006343ad649fe910a0e8922ed58cf1
Zsuró Tibor <zsurotibor@gmail.com>
2024-04-27 16:18:18 +0000

    Add component extension

--- plugin/src/main/kotlin/com/github/dgzt/mundus/plugin/ode4j/MundusOde4jPlugin.kt
+++ plugin/src/main/kotlin/com/github/dgzt/mundus/plugin/ode4j/MundusOde4jPlugin.kt
@@ -1,5 +1,9 @@
 package com.github.dgzt.mundus.plugin.ode4j
 
+import com.github.dgzt.mundus.plugin.ode4j.component.Ode4jPhysicsComponent
+import com.mbrlabs.mundus.commons.scene3d.GameObject
+import com.mbrlabs.mundus.commons.scene3d.components.Component
+import com.mbrlabs.mundus.pluginapi.ComponentExtension
 import com.mbrlabs.mundus.pluginapi.MenuExtension
 import com.mbrlabs.mundus.pluginapi.ui.RootWidget
 import org.pf4j.Extension
@@ -14,12 +18,19 @@ class MundusOde4jPlugin : Plugin() {
             const val PAD = 5f
         }
 
-        override fun getMenuName(): String = "Your plugin"
+        override fun getMenuName(): String = "Ode4j Physics"
 
         override fun setupDialogRootWidget(root: RootWidget) {
-            root.addLabel("Your plugin").setPad(PAD, PAD, PAD, PAD)
+            root.addLabel("").setPad(PAD, PAD, PAD, PAD)
         }
 
+    }
+
+    @Extension
+    class Ode4jComponentExtension : ComponentExtension {
+        override fun getComponentName(): String = "Ode4j Physics"
+
+        override fun createComponent(gameObject: GameObject): Component = Ode4jPhysicsComponent(gameObject)
     }
 
 }
---
+++ plugin/src/main/kotlin/com/github/dgzt/mundus/plugin/ode4j/component/Ode4jPhysicsComponent.kt
@@ -0,0 +1,15 @@
+package com.github.dgzt.mundus.plugin.ode4j.component
+
+import com.mbrlabs.mundus.commons.scene3d.GameObject
+import com.mbrlabs.mundus.commons.scene3d.components.AbstractComponent
+import com.mbrlabs.mundus.commons.scene3d.components.Component
+
+class Ode4jPhysicsComponent(gameObject: GameObject) : AbstractComponent(gameObject) {
+    override fun update(delta: Float) {
+        // NOOP
+    }
+
+    override fun clone(go: GameObject): Component {
+        return Ode4jPhysicsComponent(go)
+    }
+}
