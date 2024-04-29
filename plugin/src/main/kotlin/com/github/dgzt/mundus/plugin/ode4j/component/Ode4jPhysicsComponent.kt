9e00e00f200cc8aaa565d9438e12475b323b29e2
Zsuró Tibor <zsurotibor@gmail.com>
2024-04-29 15:55:16 +0000

    Set physics type on physics component

--- plugin/src/main/kotlin/com/github/dgzt/mundus/plugin/ode4j/component/Ode4jPhysicsComponent.kt
+++ plugin/src/main/kotlin/com/github/dgzt/mundus/plugin/ode4j/component/Ode4jPhysicsComponent.kt
@@ -5,6 +5,11 @@ import com.mbrlabs.mundus.commons.scene3d.components.AbstractComponent
 import com.mbrlabs.mundus.commons.scene3d.components.Component
 
 class Ode4jPhysicsComponent(gameObject: GameObject) : AbstractComponent(gameObject) {
+
+    init {
+        type = Component.Type.PHYSICS
+    }
+
     override fun update(delta: Float) {
         // NOOP
     }
