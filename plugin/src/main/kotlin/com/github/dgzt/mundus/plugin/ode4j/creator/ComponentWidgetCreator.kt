175d1a9c36888661e9ca8dfc90c56135b2d7c2a7
Zsuró Tibor <zsurotibor@gmail.com>
2024-05-11 08:44:43 +0000

    Can update geom box size via widget

--- plugin/src/main/kotlin/com/github/dgzt/mundus/plugin/ode4j/creator/ComponentWidgetCreator.kt
+++ plugin/src/main/kotlin/com/github/dgzt/mundus/plugin/ode4j/creator/ComponentWidgetCreator.kt
@@ -1,59 +1,89 @@
 package com.github.dgzt.mundus.plugin.ode4j.creator
 
+import com.badlogic.gdx.math.Vector3
 import com.badlogic.gdx.utils.Array
+import com.github.antzGames.gdx.ode4j.math.DVector3
+import com.github.antzGames.gdx.ode4j.ode.DBox
 import com.github.dgzt.mundus.plugin.ode4j.component.Ode4jPhysicsComponent
+import com.github.dgzt.mundus.plugin.ode4j.debug.DebugModelBuilder
+import com.github.dgzt.mundus.plugin.ode4j.type.ShapeType
 import com.github.dgzt.mundus.plugin.ode4j.util.GameObjectUtils
-import com.mbrlabs.mundus.commons.scene3d.components.Component
-import com.mbrlabs.mundus.commons.scene3d.components.ModelComponent
 import com.mbrlabs.mundus.pluginapi.ui.RootWidget
 import com.mbrlabs.mundus.pluginapi.ui.RootWidgetCell
 
 object ComponentWidgetCreator {
+
+    private val TMP_DVECTOR3 = DVector3()
+    private val TMP_VECTOR3 = Vector3()
 
     private const val BOX = "Box"
     private const val CYLINDER = "Cylinder"
 
     fun setup(component: Ode4jPhysicsComponent, rootWidget: RootWidget) {
         if (GameObjectUtils.isModelGameObject(component.gameObject)) {
-            val modelComponent = component.gameObject.findComponentByType<ModelComponent>(Component.Type.MODEL)
-            setupModelComponentWidget(modelComponent, rootWidget)
+            setupModelComponentWidget(component, rootWidget)
         }
     }
 
-    private fun setupModelComponentWidget(modelComponent: ModelComponent, rootWidget: RootWidget) {
+    private fun setupModelComponentWidget(component: Ode4jPhysicsComponent, rootWidget: RootWidget) {
         var innerWidgetCell: RootWidgetCell? = null
 
         val types = Array<String>()
         types.add(BOX)
-        types.add(CYLINDER)
-        rootWidget.addSelectBox(types) {
+//        types.add(CYLINDER)
+        rootWidget.addSelectBox(types, getSelectBoxType(component)) {
             innerWidgetCell!!.rootWidget!!.clearWidgets()
 
             if (it.equals(BOX)) {
-                addBoxWidgets(modelComponent, innerWidgetCell!!.rootWidget)
+                addBoxWidgets(component, innerWidgetCell!!.rootWidget)
             }
         }
         rootWidget.addRow()
 
         innerWidgetCell = rootWidget.addEmptyWidget()
 
-        // Add default box widgets
-        addBoxWidgets(modelComponent, innerWidgetCell.rootWidget)
+        if (ShapeType.BOX == component.shapeType) {
+            addBoxWidgets(component, innerWidgetCell.rootWidget)
+        }
     }
 
-    private fun addBoxWidgets(modelComponent: ModelComponent, rootWidget: RootWidget) {
-        val boundingBox = modelComponent.orientedBoundingBox.bounds
+    private fun getSelectBoxType(component: Ode4jPhysicsComponent): String {
+        // TODO support other types also
+        return BOX
+    }
+
+    private fun addBoxWidgets(component: Ode4jPhysicsComponent, rootWidget: RootWidget) {
+        val boxGeom = component.geom as DBox
+        boxGeom.getLengths(TMP_DVECTOR3)
 
         rootWidget.addLabel("Size:")
         rootWidget.addRow()
-        rootWidget.addSpinner("Width", 0.1f, Float.MAX_VALUE, boundingBox.width, 0.1f) {
-            // NOOP
+        rootWidget.addSpinner("Width", 0.1f, Float.MAX_VALUE, TMP_DVECTOR3.get0().toFloat(), 0.1f) {
+            val currentLength = boxGeom.lengths
+            boxGeom.setLengths(it.toDouble(), currentLength.get1(), currentLength.get2())
+            updateDebugInstanceIfNecessary(component, boxGeom)
         }
-        rootWidget.addSpinner("Height", 0.1f, Float.MAX_VALUE, boundingBox.height, 0.1f) {
-            // NOOP
+        rootWidget.addSpinner("Height", 0.1f, Float.MAX_VALUE, TMP_DVECTOR3.get1().toFloat(), 0.1f) {
+            val currentLength = boxGeom.lengths
+            boxGeom.setLengths(currentLength.get0(), it.toDouble(), currentLength.get2())
+            updateDebugInstanceIfNecessary(component, boxGeom)
         }
-        rootWidget.addSpinner("Depth", 0.1f, Float.MAX_VALUE, boundingBox.depth, 0.1f) {
-            // NOOP
+        rootWidget.addSpinner("Depth", 0.1f, Float.MAX_VALUE, TMP_DVECTOR3.get2().toFloat(), 0.1f) {
+            val currentLength = boxGeom.lengths
+            boxGeom.setLengths(currentLength.get0(), currentLength.get1(), it.toDouble())
+            updateDebugInstanceIfNecessary(component, boxGeom)
         }
+    }
+
+    private fun updateDebugInstanceIfNecessary(component: Ode4jPhysicsComponent, geom: DBox) {
+        if (component.debugInstance == null) return
+
+        var debugInstance = component.debugInstance
+        debugInstance.model.dispose()
+
+        val lengths = geom.lengths
+        debugInstance = DebugModelBuilder.createBox(lengths.get0().toFloat(), lengths.get1().toFloat(), lengths.get2().toFloat())
+        debugInstance.transform.setTranslation(component.gameObject.getPosition(TMP_VECTOR3))
+        component.debugInstance = debugInstance
     }
 }
